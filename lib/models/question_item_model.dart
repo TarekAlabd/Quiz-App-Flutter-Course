97aafef4e5c152de7f24f1d59260ad2315a880f4
Tarek Alabd <tarekalabd1@gmail.com>
2023-02-14 00:17:52 +0200

    feat: calculate the total score and show it

--- lib/models/question_item_model.dart
+++ lib/models/question_item_model.dart
@@ -1,12 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/models/answer_item_model.dart';
+
 class QuestionItemModel {
   final String title;
+  final List<AnswerItemModel> availableAnswers;
 
-  QuestionItemModel({required this.title});
+  QuestionItemModel({required this.title, required this.availableAnswers});
 }
 
 List<QuestionItemModel> questions = [
-  QuestionItemModel(title: 'What is your favorite sport?'),
-  QuestionItemModel(title: 'What is your favorite color?'),
-  QuestionItemModel(title: 'What is your favorite animal?'),
-  QuestionItemModel(title: 'What is your favorite bird?'),
+  QuestionItemModel(
+    title: 'What is your favorite sport?',
+    availableAnswers: [
+      AnswerItemModel(
+        title: 'Football',
+        onPressed: () => debugPrint('Football Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Basketball',
+        onPressed: () => debugPrint('Basketball Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Volleyball',
+        onPressed: () => debugPrint('Volleyball Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Kickboxing',
+        onPressed: () => debugPrint('Kickboxing Choice'),
+      ),
+    ],
+  ),
+  QuestionItemModel(
+    title: 'What is your favorite color?',
+    availableAnswers: [
+      AnswerItemModel(
+        title: 'White',
+        onPressed: () => debugPrint('White Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Red',
+        onPressed: () => debugPrint('Red Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Green',
+        onPressed: () => debugPrint('Green Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Blue',
+        onPressed: () => debugPrint('Blue Choice'),
+      ),
+    ],
+  ),
+  QuestionItemModel(
+    title: 'What is your favorite animal?',
+    availableAnswers: [
+      AnswerItemModel(
+        title: 'Lion',
+        onPressed: () => debugPrint('Lion Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Camel',
+        onPressed: () => debugPrint('Camel Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Tiger',
+        onPressed: () => debugPrint('Tiger Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Cat',
+        onPressed: () => debugPrint('Cat Choice'),
+      ),
+    ],
+  ),
+  QuestionItemModel(
+    title: 'What is your favorite bird?',
+    availableAnswers: [
+      AnswerItemModel(
+        title: 'Flutter',
+        onPressed: () => debugPrint('Flutter Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Owl',
+        onPressed: () => debugPrint('Owl Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Pigeon',
+        onPressed: () => debugPrint('Pigeon Choice'),
+      ),
+      AnswerItemModel(
+        title: 'Raven',
+        onPressed: () => debugPrint('Raven Choice'),
+      ),
+    ],
+  ),
 ];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,44 +12,72 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int questionIndex = 0;
+  int myTotalScore = 0;
+  int resultedScore = questions.length * 10;
+  bool showTotalScore = false;
 
   @override
   Widget build(BuildContext context) {
+    debugPrint('Total Score: $myTotalScore');
+    showTotalScore = myTotalScore >= resultedScore;
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 16, bottom: 16),
-            child: Text(
-              questions[questionIndex].title,
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w500,
-                color: Colors.blue,
+      body: !showTotalScore
+          ? Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 16),
+                  child: Text(
+                    questions[questionIndex].title,
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: questions[questionIndex]
+                      .availableAnswers
+                      .map(
+                        (answerMap) => AnswerItem(
+                          answerMap: answerMap,
+                          questionIndexChangeCallback: () {
+                            if (questionIndex + 1 < questions.length) {
+                              setState(() {
+                                questionIndex += 1;
+                              });
+                            }
+                            setState(() {
+                              myTotalScore += 10;
+                            });
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+              ],
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Congratulations!',
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'Your total score is: $myTotalScore',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ],
               ),
             ),
-          ),
-          Column(
-            children: answersForFirstQuestion
-                .map(
-                  (answerMap) => AnswerItem(
-                    answerMap: answerMap,
-                    questionIndexChangeCallback: () {
-                      if (questionIndex + 1 < questions.length) {
-                        setState(() {
-                          questionIndex += 1;
-                        });
-                      }
-                    },
-                  ),
-                )
-                .toList(),
-          ),
-        ],
-      ),
     );
   }
 }
